94c53e22f0645b114bd8c7799de2bfa413e51ea7
opetro <opetro@nyc.gr>
2024-03-04 19:00:16 +0200

    Update

--- src/Json/ChuckNorris.java
+++ src/Json/ChuckNorris.java
@@ -1,5 +1,11 @@
 package Json;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -7,8 +13,16 @@ public class ChuckNorris {
     public static void main(String[] args) {
         try {
             URL url= new URL("https://api.chucknorris.io/jokes/random");
+            InputStreamReader is = new InputStreamReader(url.openStream());
+            JSONParser parser = new JSONParser();
+            JSONObject json=(JSONObject) parser.parse(is);
+            System.out.println(json);
         } catch (MalformedURLException e) {
             System.out.println("URL has a problem!");
+        } catch (IOException e) {
+            System.out.println("IO Problem");
+        } catch (ParseException e) {
+            System.out.println("Problem parsing from URL");
         }
     }
 }
